8c595ce69a02fbe9fa889353a9081f7cd2f14c82
ArchyScript <yungdansa@gmail.com>
2022-08-30 11:11:29 +0100

    ...

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,35 +1,35 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router'
 import { RouteRecordRaw } from 'vue-router'
 
-const MemoriesPage = () => import('@/pages/MemoriesPage.vue')
-const MemoryDetailsPage = () => import('@/pages/MemoryDetailsPage.vue')
-const AddMemoryPage = () => import('@/pages/AddMemoryPage.vue')
+// const MemoriesPage = () => import('@/pages/MemoriesPage.vue')
+// const MemoryDetailsPage = () => import('@/pages/MemoryDetailsPage.vue')
+// const AddMemoryPage = () => import('@/pages/AddMemoryPage.vue')
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    redirect: '/memories',
-  },
-  {
-    path: '/memories',
-    component: MemoriesPage,
-  },
-  {
-    path: '/memories/:id',
-    component: MemoryDetailsPage,
-  },
-  {
-    path: '/memories/add',
-    component: AddMemoryPage,
-  },
   // {
-  //   path: '',
-  //   redirect: '/folder/Inbox'
+  //   path: '/',
+  //   redirect: '/memories',
   // },
   // {
-  //   path: '/folder/:id',
-  //   component: () => import ('../views/FolderPage.vue')
-  // }
+  //   path: '/memories',
+  //   component: MemoriesPage,
+  // },
+  // {
+  //   path: '/memories/:id',
+  //   component: MemoryDetailsPage,
+  // },
+  // {
+  //   path: '/memories/add',
+  //   component: AddMemoryPage,
+  // },
+  {
+    path: '',
+    redirect: '/folder/Inbox'
+  },
+  {
+    path: '/folder/:id',
+    component: () => import ('../views/FolderPage.vue')
+  }
 ]
 
 const router = createRouter({
